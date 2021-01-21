8db3b3e525ebee912f98c1bfb923e55bdbb1f046
Deborah Ho <debho@umich.edu>
2021-01-21 22:39:22 +0000

    added ref files extracted EEG and axy data into dfs

---
+++ README.md
@@ -0,0 +1,8 @@
+Tasks:
+1. extract data using Python
+2. identify data types
+3. plot them using time as x-axis
+
+Notes:
+1. EEG data sampled at 250Hz
+2. axy data sampled at 250Hz
--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 
 #imports CSV file as a dataframe
 df = pd.read_csv('hrdata.csv', header = None)
-df.reset_index()
-
 #transpose rows and cols
 df = df.T
+df.reset_index()
 
 #rename cols
-df = df.rename(columns = {0 : 'data',
-                1 : 'type'})
+df = df.rename(columns = {0 : 'value',
+                        1 : 'type'})
 
-#creates dictionary for data types
+#for easy reference
 datatypes = {
             'typenum' : [0, 1, 2, 3, 4, 5, 6, 7, 8,
                          9, 10, 11, 12, 13, 14, 15],
@@ -35,9 +34,25 @@ datatypes = {
                         'Version']
                         }
 
-#prints df
-print(df)
+typetable = pd.DataFrame(datatypes)
+print(typetable)
+
+#extract EEG data
+eeg = df[
+    (df['type'] >= 2) &
+    (df['type'] <= 5)
+]
+
+print(eeg)
+
+#extract axy data
+axy = df[
+    (df['type'] >= 7) &
+    (df['type'] <= 12)
+]
+print(axy)
+
 
 #STUFF TO DO:
-
+#add column that shows type (mapped to dictionary)
 #plot graphs
